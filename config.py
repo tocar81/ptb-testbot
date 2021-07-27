baed94451d7d748225e5a00f3ea90d2f24f69f80
Tony <tocar81@gmail.com>
2021-07-26 23:12:46 -0500

    thinks ready

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
-TOKEN = os.environ[TOKEN]
-PORT = int(os.environ.get('port':5000))
+TOKEN = os.environ['TOKEN']
+PORT = int(os.environ.get('PORT', 5000))
--- main.py
+++ main.py
@@ -1,43 +1,47 @@
 import logging
 from telegram.ext import Updater, MessageHandler, CommandHandler, Filters, ConversationHandler
 import telegram
-import config
+from config import TOKEN, PORT
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
+)
 logger = logging.getLogger()
-if __name__=="__main__"
-    my_bot = telegram.Bot(token=config.TOKEN)
+if __name__ == "__main__":
+    my_bot = telegram.Bot(token=TOKEN)
     updater = Updater(my_bot.token)
     print(my_bot.getMe())
-    dp = updater.dispatcher
 
-REPETIR=0
 
-def start_cback(update, context):
-    update.message.reply_text("Estoy vivo")
+REPETIR = 0
+
+
+def start_callback(update, context):
+    update.message.reply_text("Estoy vivo ya en HEROKU...")
     return 0
-def repite_cback(update, context):
+
+
+def repite_callback(update, context):
     update.message.reply_text("Mandame algo para repetir:")
     return REPETIR
-def arepetir_cback(update, context):
+
+
+def arepetir_callback(update, context):
     mid = update.message.from_user.id
     context.bot.send_message(
         chat_id=mid,
         parse_mode='MarkdownV2',
-        text=update.message.text
+        text=update.message.text,
     )
 
-dp.add_handler(CommandHandler('start',start_cback))
+dp = updater.dispatcher
+dp.add_handler(CommandHandler('start', start_callback))
 dp.add_handler(ConversationHandler(
-    entry_points=[CommandHandler('repite',repite_cback)],
+    entry_points=[CommandHandler('repite', repite_callback)],
     states={
-        REPETIR: [MessageHandler(Filters.text, arepetir_cback)]
+        REPETIR: [MessageHandler(Filters.text, arepetir_callback)]
     },
     fallbacks=[]
 ))
-updater.start_webhook("0.0.0.0", config.PORT, config.TOKEN, webhook_url="https://examensito-bot.herokuapp.com/" + TOKEN)
-updater.idle()>>>>>>>
-
-
+updater.start_webhook("0.0.0.0", PORT, TOKEN, webhook_url="https://examensito-bot.herokuapp.com/" + TOKEN)
+updater.idle()
